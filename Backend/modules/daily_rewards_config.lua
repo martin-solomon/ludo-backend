56fdb40b79352b8330aa2e9f12358c048f4a5059
M-vignesh08 <m.vignesh1408@gmail.com>
2026-01-13 00:47:49 +0530

    Delete Backend/modules/daily_rewards_config.lua

--- Backend/modules/daily_rewards_config.lua
+++
@@ -1,9 +0,0 @@
-return {
-  [1] = { coins = 10 },
-  [2] = { coins = 20 },
-  [3] = { coins = 30 },
-  [4] = { coins = 40 },
-  [5] = { coins = 50 },
-  [6] = { coins = 60 },
-  [7] = { coins = 70 }
-}
