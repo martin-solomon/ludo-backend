48c9977d21b24f2a0d90f772e90d7406ae0df68f
M-vignesh08 <m.vignesh1408@gmail.com>
2026-01-12 23:32:59 +0530

    Add files via upload

---
+++ Backend/modules/daily_rewards_config.lua
@@ -0,0 +1,9 @@
+return {
+  [1] = { coins = 10 },
+  [2] = { coins = 20 },
+  [3] = { coins = 30 },
+  [4] = { coins = 40 },
+  [5] = { coins = 50 },
+  [6] = { coins = 60 },
+  [7] = { coins = 70 }
+}
