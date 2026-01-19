b2d8b7936afe8a630e9258022315a1f5ba285062
M-vignesh08 <m.vignesh1408@gmail.com>
2026-01-19 21:40:43 +0530

    Update rpc_quick_join.lua

--- Backend/modules/rpc_quick_join.lua
+++ Backend/modules/rpc_quick_join.lua
@@ -1,8 +1,8 @@
 local nk = require("nakama")
 
-local function rpc_quick_join(context, payload)
+local function rpc_match_join(context, payload)
   if not context.user_id then
-    return nk.json_encode({ error = "NO_SESSION" }), 401
+    return nk.json_encode({ error = "NO_SESSION" })
   end
 
   local input = {}
@@ -14,15 +14,15 @@ local function rpc_quick_join(context, payload)
   end
 
   if not input.match_id then
-    return nk.json_encode({ error = "MATCH_ID_REQUIRED" }), 400
+    return nk.json_encode({ error = "MATCH_ID_REQUIRED" })
   end
 
-  -- IMPORTANT:
-  -- DO NOT join match here
-  -- Just return match_id
+  nk.match_join(input.match_id, context.user_id, context.session_id)
+
   return nk.json_encode({
+    status = "JOINED",
     match_id = input.match_id
-  }), 200
+  })
 end
 
-nk.register_rpc(rpc_quick_join, "match_quick_join")
+nk.register_rpc(rpc_match_join, "match_join")
