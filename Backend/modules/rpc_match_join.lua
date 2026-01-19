23093b5249f4ebcab3f3e08645fb5cb996115afc
M-vignesh08 <m.vignesh1408@gmail.com>
2026-01-19 22:00:16 +0530

    Update rpc_match_join.lua

--- Backend/modules/rpc_match_join.lua
+++ Backend/modules/rpc_match_join.lua
@@ -2,24 +2,17 @@ local nk = require("nakama")
 
 local function rpc_match_join(context, payload)
     if not context.user_id then
-        return nk.json_encode({ error = "NO_SESSION" })
+        return nk.json_encode({ error = "NO_SESSION" }), 401
     end
 
     local input = nk.json_decode(payload or "{}")
     if not input.match_id then
-        return nk.json_encode({ error = "MATCH_ID_REQUIRED" })
+        return nk.json_encode({ error = "MATCH_ID_REQUIRED" }), 400
     end
 
-    nk.match_join(
-        input.match_id,
-        context.user_id,
-        context.session_id
-    )
+    nk.match_join(input.match_id, context.user_id, context.session_id)
 
-    return nk.json_encode({
-        status = "JOINED",
-        match_id = input.match_id
-    })
+    return nk.json_encode({ status = "JOINED" })
 end
 
 nk.register_rpc(rpc_match_join, "match_join")
